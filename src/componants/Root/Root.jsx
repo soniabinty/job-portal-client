b7996cceed1457123685e0660f0e1dc34c4c0956
Sonia Binty <sonianazninbinty@gmail.com>
2024-12-20 02:13:25 +0600

    first commit

---
+++ src/componants/Root/Root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Shared/Navbar';
+
+const Root = () => {
+  return (
+    <div className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      
+    </div>
+  );
+};
+
+export default Root;
